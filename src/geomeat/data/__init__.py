25902a1c73a8c10ae2d5405fbb93bc4c0907dfd3
Achilles Rasquinha <achillesrasquinha@gmail.com>
2021-10-09 15:19:51 -0500

    ci: run bpyutils

--- src/geomeat/data/__init__.py
+++ src/geomeat/data/__init__.py
@@ -2,11 +2,14 @@ import os.path as osp
 import csv
 
 from geomeat.config import PATH
+from bpyutils.util.system import ShellEnvironment
 
 def get_data():
     path_data = osp.join(PATH["DATA"], "data.csv")
     
     with open(path_data) as f:
         data  = csv.reader(f)
-        for row in data:
-            pass
+
+        with ShellEnvironment() as shell:
+            for row in data:
+                print(row)

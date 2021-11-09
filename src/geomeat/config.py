e9198b930ab73cef3b518113c1bfc23d2fb25de4
Achilles Rasquinha <achillesrasquinha@gmail.com>
2021-11-08 23:00:19 -0600

    fix: add path cache for enviornment variable

--- src/geomeat/config.py
+++ src/geomeat/config.py
@@ -1,12 +1,14 @@
 import os.path as osp
 
 from geomeat.__attr__ import __name__ as NAME
+from geomeat.const import CONST
 
 from bpyutils.config      import get_config_path
-from bpyutils.util.system import pardir
+from bpyutils.util.system  import pardir
+from bpyutils.util.environ import getenv
 
 PATH = dict()
 
 PATH["BASE"]  = pardir(__file__, 1)
 PATH["DATA"]  = osp.join(PATH["BASE"], "data")
-PATH["CACHE"] = get_config_path(NAME)
+PATH["CACHE"] = getenv("PATH_CACHE", get_config_path(NAME), prefix = CONST["prefix"])

7e6547473bf77ae3a25f953e4b56932a9889e9a5
Achilles Rasquinha <achillesrasquinha@gmail.com>
2021-11-23 16:26:37 -0600

    fix: fix trim

--- src/s3mart/data/__init__.py
+++ src/s3mart/data/__init__.py
@@ -8,7 +8,9 @@ from s3mart import settings, __name__ as NAME
 from bpyutils.util._csv    import read as read_csv
 from bpyutils.util.ml      import get_data_dir
 from bpyutils.util.types   import build_fn
-from bpyutils import parallel, log
+from bpyutils.util.string  import check_url, safe_decode
+from bpyutils.util.system  import write
+from bpyutils import parallel, log, req
 
 from s3mart.data.functions import (
     get_fastq,
@@ -24,7 +26,18 @@ logger = log.get_logger(name = NAME)
 CACHE  = PATH["CACHE"]
 
 def get_input_data(input = None, data_dir = None, *args, **kwargs):
+    data_dir = get_data_dir(NAME, data_dir)
+
     if input:
+        if check_url(input):
+            response = req.get(input)
+            response.raise_for_status()
+
+            content  = response.content
+
+            input    = osp.join(data_dir, "input.csv")
+            write(input, safe_decode(content))
+        
         input = osp.abspath(input)
 
         if osp.isdir(input):

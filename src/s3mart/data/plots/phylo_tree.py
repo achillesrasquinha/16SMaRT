83309a15ffde84a021551bb2dc93b1e4a012ed43
Achilles Rasquinha <achillesrasquinha@gmail.com>
2021-12-09 21:51:04 -0600

    fix: add plots

--- src/s3mart/data/plots/phylo_tree.py
+++ src/s3mart/data/plots/phylo_tree.py
@@ -1,33 +1,29 @@
+import rpy2.robjects as ro
 from rpy2.robjects.packages import importr
+
+from bpyutils import log, parallel
 
 from s3mart.data.plots.util import save_plot
 from s3mart import settings
 
+from s3mart import __name__ as NAME
+
+logger = log.get_logger(name = NAME)
+
+R = ro.r
+
 def plot(*args, **kwargs):
     mothur_data = kwargs["mothur_data"]
 
-    ggt = importr("ggtree")
+    base    = importr("base")
+    treeio  = importr("treeio")
+    ggt     = importr("ggtree")
 
-    # ggt.ggtree()
-    
-    # pseq_data   = pseq.import_mothur(
-    #     mothur_shared_file  = mothur_data["shared"],
-    #     # mothur_list_file    = mothur_data["list"],
-    #     mothur_tree_file    = mothur_data["tree"],
-    #     # mothur_constaxonomy_file = mothur_data["taxonomy"],
-    #     # mothur_group_file   = mothur_data["group"],
-    #     cutoff = settings.get("cutoff_level")
-    # )
+    logger.info("Loading tree...")
 
-    # pseq_data_resampled = normalize_pseq(pseq_data)
-    
-    # plot_kwargs = { "width": 10, "height": 8 }
+    tree_data = treeio.read_newick(mothur_data["tree"])
 
-    # kwargs["plot_kwargs"] = plot_kwargs
-    
-    # ordinate_data = pseq.ordinate(pseq_data)
-    # plot = pseq.plot_ordination(pseq_data, ordinate_data, type = "samples")
-    # save_plot(plot, *args, **kwargs)
+    logger.info("Rendering tree...")
 
-    # plot = pseq.plot_ordination(pseq_data_resampled, ordinate_data, type = "samples")
-    # save_plot(plot, suffix = "resampled", *args, **kwargs)
+    plot = ggt.ggtree(tree_data)
+    save_plot(plot, *args, **kwargs)
